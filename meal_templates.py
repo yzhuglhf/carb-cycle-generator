b98a23b3d2983a986c88dec50e3c9fae1d49aa06
yzhuglhf <yingbing.zhu@hotmail.com>
2026-01-08 21:16:51 -0800

    Add meal templates

---
+++ meal_templates.py
@@ -0,0 +1,37 @@
+BREAKFAST = [
+    "1 egg",
+    "1 latte (no sugar)",
+    "Optional: small fruit portion"
+]
+
+LOW_CARB_MEALS = {
+    "lunch": [
+        "Chicken breast 150g + broccoli",
+        "Fish 120g + spinach",
+        "Shrimp 150g + cucumber",
+    ],
+    "dinner": [
+        "Fish 120g",
+        "Greek yogurt 150g",
+    ],
+}
+
+MID_CARB_MEALS = {
+    "lunch": [
+        "Chicken 120g + sweet potato 100g",
+        "Fish 120g + rice 100g",
+    ],
+    "dinner": [
+        "Egg + vegetables",
+    ],
+}
+
+HIGH_CARB_MEALS = {
+    "lunch": [
+        "Short ribs 150g + rice 150g",
+        "Chicken thigh (skinless) 150g + sweet potato 150g",
+    ],
+    "dinner": [
+        "Chicken breast 120g + vegetables",
+    ],
+}
